4ebf6a7435f2e163d2d740ec417a38a24a0f955d
Ellouminati <abdellaziz.elloumi@esprit.tn>
2023-04-26 20:28:42 +0100

    login fonctionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './FrontOffice/landing-page/landing-page.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import { ForbiddenComponent } from './forbidden/forbidden.component';
+import { FormsModule } from  '@angular/forms';
+import { RegisterComponent } from './register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,12 +31,15 @@ import { HttpClientModule } from '@angular/common/http';
     DormitoriesComponent,
     DashboardComponent,
     LoginComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    ForbiddenComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
