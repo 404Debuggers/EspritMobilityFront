cc5d53f2f25f7b77e6247700e1b8263701eae251
bayrem999 <bayrem.hamdi@esprit.tn>
2023-05-10 00:28:56 +0100

    uni complet

---
+++ src/app/class/university.ts
@@ -0,0 +1,10 @@
+export class University {
+    universiteId: any
+  nameUniversite: any
+  location: any
+  lattitude: any
+  longitude: any
+  description: any
+  type: any
+  image : any
+}
--- src/app/university/university.component.ts
+++ src/app/university/university.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UniversityService } from '../shared/university.service';
+import { University } from '../class/university';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-university',
@@ -7,8 +9,9 @@ import { UniversityService } from '../shared/university.service';
   styleUrls: ['./university.component.css']
 })
 export class UniversityComponent implements OnInit {
-  universities :any;
-
+  @ViewChild('f') univform!: NgForm;
+  universities: University[] = [];
+university!:University;
   form : boolean = true;
   universiteId: any
   nameUniversite: any
@@ -18,6 +21,7 @@ export class UniversityComponent implements OnInit {
   description: any
   type: any
   image : any
+  editMode: boolean = false;
   
   
 
@@ -26,19 +30,50 @@ export class UniversityComponent implements OnInit {
 
   ngOnInit(): void {
     this.getAllUniv();
+    
 
     
   }
   getAllUniv(){
     this.universityservice.getAllunis().subscribe((res:any)=>this.universities=res);
   }
-
+  
+  
   addUni(university: any) {
     //console.log(p.value)
-    this.universityservice.addUni(university.value).subscribe()
-    this.getAllUniv()
+    if(!this.editMode){
+    this.universityservice.addUni(university.value).subscribe(() => {
+      // update ListInterview with the new interview
+      this.universityservice
+        .getAllunis()
+        .subscribe((res: any) => {
+          this.universities = res;
+        });
+    });
+    }
+    else{
+      
+      this.universityservice.editUni(this.univform.value).subscribe(() => {
+        const index = this.universities.findIndex(
+          (i) => i.universiteId === this.university?.universiteId
+        );
+        if (index !== -1) {
+          this.universities[index] = this.university;
+        }
+        this.editMode = false;
+        this.university = new University();
+        this.univform.resetForm();
+        this.universityservice
+        .getAllunis()
+        .subscribe((res: any) => {
+          this.universities = res;
+        });
+      })
+      
+  }
+    }
 
-}
+
 
 suppUni(universiteId: any){
   console.log('Offer ID:', universiteId);
@@ -49,5 +84,36 @@ suppUni(universiteId: any){
 }
 
 
+OnEditClickedd(id: any) {
+  let currentUniv = this.universities.find((p:any) => {
+    return p.universiteId === id;
+  });
+  
+  this.univform.form.patchValue({
+    universiteId:currentUniv?.universiteId,
+    nameUniversite: currentUniv?.nameUniversite,
+    location: currentUniv?.location,
+    lattitude: currentUniv?.lattitude,
+    longitude: currentUniv?.longitude,
+    description: currentUniv?.description,
+    type: currentUniv?.type
+    
+  });
 
+  this.editMode = true;
+}
+switch () {
+  this.editMode = false
+  this.university = {
+    universiteId: null,
+nameUniversite: null,
+location: null,
+lattitude: null, 
+longitude: null,
+description: null,
+type: null,
+image : null
+    
+  };
+}
 }
